244b6de7b2093bad5b0d9343fcde8b5868f40ebe
JosephByronLane <24197141@modelo.edu.mx>
2025-04-09 20:36:40 -0600

    push

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
     // Optional: Add any additional Next.js configuration options here
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
   };
   module.exports = nextConfig;
